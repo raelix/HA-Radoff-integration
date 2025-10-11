92a2097d3762b8e3acb3bdbc2bc2d298b5f63c50
raelix <raelix@hotmail.it>
2025-10-11 16:09:10 +0200

    added default aws cognito for Radoff API

--- custom_components/radoff/const.py
+++ custom_components/radoff/const.py
@@ -6,3 +6,8 @@ CONF_POOL_REGION = "pool_region"
 DEFAULT_SCAN_INTERVAL = 60
 MIN_SCAN_INTERVAL = 10
 CONF_INDEX = "generate_index"
+
+# AWS Cognito defaults for Radoff API
+DEFAULT_POOL_ID = "eu-west-1_zD4CSIZ6i"
+DEFAULT_POOL_REGION = "eu-west-1"
+DEFAULT_CLIENT_ID = "61ckd0c4qoq0ov7mmphrj7kstj"
