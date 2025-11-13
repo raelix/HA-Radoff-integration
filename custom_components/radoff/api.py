09c3ec5898f5ad3f407587ee0747aa4883949ab9
Carsten Schiers <carsten@schiers.de>
2025-11-13 17:38:14 +0100

    Update api.py
    Add fixed unit of measurement for TVOC

--- custom_components/radoff/api.py
+++ custom_components/radoff/api.py
@@ -13,6 +13,7 @@ from requests.adapters import HTTPAdapter, Retry
 import time
 
 from homeassistant.components.sensor import DEVICE_CLASS_UNITS, SensorDeviceClass
+from homeassistant.const import UnitOfVolatileOrganicCompounds 
 from homeassistant.const import UnitOfPressure, UnitOfTemperature
 
 _LOGGER = logging.getLogger(__name__)
@@ -48,9 +49,7 @@ MAPPING: dict[str, dict[str, dict[str, Any]]] = {
         "tvoc": {
             "deviceClass": SensorDeviceClass.VOLATILE_ORGANIC_COMPOUNDS,
             "friendlyName": "VOC",
-            "unit": list(
-                DEVICE_CLASS_UNITS[SensorDeviceClass.VOLATILE_ORGANIC_COMPOUNDS]
-            )[0],
+            "unit": UnitOfVolatileOrganicCompounds.MICROGRAMS_PER_CUBIC_METER,
             "normalize_fn": lambda value: round(float(value),1),  # V-lx → µg/m³
         },
         "eco2": {
